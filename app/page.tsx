e1e351219a24df00d70da03108904c3e62bca948
angeldev <baezdev@gmail.com>
2025-01-30 01:42:13 -0600

    Refactor page layout to integrate Nav component and update styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,79 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import css from "./page.module.css";
+import { Nav } from "@/modules/landing/components/Nav";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <main>
+      <Nav />
+      <header className="">
+        <div className="">
+          <div>
+            <h1 className="t">Con QWERTY ¡Olvídate de olvidar!</h1>
+          </div>
+          <p className="">
+            Protege y accede a tus contraseñas con un clic. La seguridad nunca
+            fue tan sencilla.
+          </p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/images/gmail.svg"
+            alt="Imagen de un logo de Gmail"
+            width={70}
+            height={70}
+            className=""
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src="/images/instagram.svg"
+            alt="Imagen de un logo de instagram"
+            width={70}
+            height={70}
+            className=""
           />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+            src="/images/netflix.svg"
+            alt="Imagen de un logo de netflix"
+            width={70}
+            height={70}
+            className=""
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <Image
+            src="/images/finger-lock-protect-svgrepo-com.svg"
+            alt="Imagen de una huella con un candado en el centro, represetando seguridad"
+            width={240}
+            height={240}
+          />
+          <Image
+            src="/images/outlook.svg"
+            alt="Imagen de un logo de outlook"
+            width={70}
+            height={70}
+            className=""
+          />
+          <Image
+            src="/images/paypal.svg"
+            alt="Imagen de un logo de paypal"
+            width={70}
+            height={70}
+            className=""
+          />
+          <Image
+            src="/images/pc.svg"
+            alt="Imagen de un logo de pc"
+            width={70}
+            height={70}
+            className=""
+          />
+        </div>
+        {/* <div className="flex justify-center mt-14 gap-8">
+         <Button variant="primary">Empezemos</Button> 
+          <button className="bg-gradient-to-r from-[--primary-light] to-[--secondary-light] text-[--secondary] flex gap-x-1 font-semibold rounded-full px-4 py-3 text-lg cursor-pointer hover:from-[--secondary] hover:to-[--secondary] transition-all duration-200">
+            <UserIcon />
+            Saber más
+          </button>
+        </div> */}
+      </header>
+    </main>
   );
 }
