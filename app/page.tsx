ebc662159709aeb022ae3c8ea7e4879fafdca71a
baezdev <baezdev@gmail.com>
2025-01-31 01:28:50 -0600

    Add Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,78 +1,83 @@
 import Image from "next/image";
 import css from "./page.module.css";
 import { Nav } from "@/modules/landing/components/Nav";
+import { Button } from "@/modules/core/design-system/Button";
+import { ArrowRight } from "@/assets/icons";
 
 export default function Home() {
   return (
     <main>
       <Nav />
-      <header className="">
-        <div className="">
+      <header className={css.Header}>
+        <div className={css.Header_slogan}>
           <div>
-            <h1 className="t">Con QWERTY ¡Olvídate de olvidar!</h1>
+            <h1>Con PassFort ¡Olvídate de olvidar!</h1>
           </div>
-          <p className="">
-            Protege y accede a tus contraseñas con un clic. La seguridad nunca
-            fue tan sencilla.
+          <p>
+            Protege y accede a tus contraseñas en un solo lugar y con un clic.
+            La seguridad nunca fue tan sencilla.
           </p>
         </div>
-        <div className="">
-          <Image
-            src="/images/gmail.svg"
-            alt="Imagen de un logo de Gmail"
-            width={70}
-            height={70}
-            className=""
-          />
-          <Image
-            src="/images/instagram.svg"
-            alt="Imagen de un logo de instagram"
-            width={70}
-            height={70}
-            className=""
-          />
-          <Image
-            src="/images/netflix.svg"
-            alt="Imagen de un logo de netflix"
-            width={70}
-            height={70}
-            className=""
-          />
+        <div className={css.Header_image}>
+          <div className={`${css.Header_logos} ${css.Header_logos__left}`}>
+            <Image
+              src="/images/gmail.svg"
+              alt="Imagen de un logo de Gmail"
+              width={70}
+              height={70}
+              className=""
+            />
+            <Image
+              src="/images/instagram.svg"
+              alt="Imagen de un logo de instagram"
+              width={70}
+              height={70}
+              className=""
+            />
+            <Image
+              src="/images/netflix.svg"
+              alt="Imagen de un logo de netflix"
+              width={70}
+              height={70}
+              className=""
+            />
+          </div>
           <Image
             src="/images/finger-lock-protect-svgrepo-com.svg"
             alt="Imagen de una huella con un candado en el centro, represetando seguridad"
             width={240}
             height={240}
           />
-          <Image
-            src="/images/outlook.svg"
-            alt="Imagen de un logo de outlook"
-            width={70}
-            height={70}
-            className=""
-          />
-          <Image
-            src="/images/paypal.svg"
-            alt="Imagen de un logo de paypal"
-            width={70}
-            height={70}
-            className=""
-          />
-          <Image
-            src="/images/pc.svg"
-            alt="Imagen de un logo de pc"
-            width={70}
-            height={70}
-            className=""
-          />
+          <div className={`${css.Header_logos} ${css.Header_logos__right}`}>
+            <Image
+              src="/images/outlook.svg"
+              alt="Imagen de un logo de outlook"
+              width={70}
+              height={70}
+              className=""
+            />
+            <Image
+              src="/images/paypal.svg"
+              alt="Imagen de un logo de paypal"
+              width={70}
+              height={70}
+              className=""
+            />
+            <Image
+              src="/images/pc.svg"
+              alt="Imagen de un logo de pc"
+              width={70}
+              height={70}
+              className=""
+            />
+          </div>
         </div>
-        {/* <div className="flex justify-center mt-14 gap-8">
-         <Button variant="primary">Empezemos</Button> 
-          <button className="bg-gradient-to-r from-[--primary-light] to-[--secondary-light] text-[--secondary] flex gap-x-1 font-semibold rounded-full px-4 py-3 text-lg cursor-pointer hover:from-[--secondary] hover:to-[--secondary] transition-all duration-200">
-            <UserIcon />
-            Saber más
-          </button>
-        </div> */}
+        <div className={css.Header_actions}>
+          <Button>
+            Iniciar <ArrowRight />
+          </Button>
+          <Button variant="secondary">Saber más</Button>
+        </div>
       </header>
     </main>
   );
