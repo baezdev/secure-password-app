2616d972cfc58ea71c460e8902d8713f8897ba27
baezdev <baezdev@gmail.com>
2025-02-06 00:00:42 -0600

    Remove h1 and styles and add new component heading

--- modules/landing/components/Header/Header.tsx
+++ modules/landing/components/Header/Header.tsx
@@ -6,6 +6,7 @@ import { ArrowRight, CircleArrowsBottomIcon } from "@/assets/icons";
 
 import css from "./Header.module.css";
 import { useThemeStore } from "@/modules/core/context/Theme";
+import { Heading } from "@/modules/core/design-system/Heading/Heading";
 
 export function Header() {
   const { theme } = useThemeStore();
@@ -13,13 +14,14 @@ export function Header() {
   const bgImage = `url("${
     theme === "light" ? "/images/bg.svg" : "/images/bg-dark.svg"
   }"), linear-gradient(to bottom, var(--header-bg) 55%, var(--background) 100%)`;
-  
 
   return (
     <header className={css.Header} style={{ background: bgImage }}>
       <div className={css.Header_slogan}>
         <div>
-          <h1>Con PassFort ¡Olvídate de olvidar!</h1>
+          <Heading variant="h1" size="large">
+            Con PassFort ¡Olvídate de olvidar!
+          </Heading>
         </div>
         <p>
           Protege y accede a tus contraseñas en un solo lugar y con un clic. La
