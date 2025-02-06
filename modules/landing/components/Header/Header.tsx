1f127b3349c6b5dff85f7618af0bf7d442624eeb
baezdev <baezdev@gmail.com>
2025-02-05 18:38:07 -0600

    Change image header depending the theme color

--- modules/landing/components/Header/Header.tsx
+++ modules/landing/components/Header/Header.tsx
@@ -1,12 +1,22 @@
+"use client";
+
 import { HeaderImage } from "./HeaderImage";
 import { Button } from "@/modules/core/design-system/Button";
-import { ArrowRight } from "@/assets/icons";
+import { ArrowRight, CircleArrowsBottomIcon } from "@/assets/icons";
 
 import css from "./Header.module.css";
+import { useThemeStore } from "@/modules/core/context/Theme";
 
 export function Header() {
+  const { theme } = useThemeStore();
+
+  const bgImage = `url("${
+    theme === "light" ? "/images/bg.svg" : "/images/bg-dark.svg"
+  }"), linear-gradient(to bottom, var(--header-bg) 55%, var(--background) 100%)`;
+  
+
   return (
-    <header className={css.Header}>
+    <header className={css.Header} style={{ background: bgImage }}>
       <div className={css.Header_slogan}>
         <div>
           <h1>Con PassFort ¡Olvídate de olvidar!</h1>
@@ -18,10 +28,12 @@ export function Header() {
       </div>
       <HeaderImage />
       <div className={css.Header_actions}>
-        <Button>
+        <Button ariaLabel="Iniciar sesión">
           Iniciar <ArrowRight />
         </Button>
-        <Button variant="secondary">Saber más</Button>
+        <Button variant="secondary" ariaLabel="Saber más">
+          Saber más <CircleArrowsBottomIcon />
+        </Button>
       </div>
     </header>
   );
