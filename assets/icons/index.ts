bcd1230b0693221719928bad35232d1b08baf74e
baezdev <baezdev@gmail.com>
2025-02-07 00:21:18 -0600

    Add bolt icon

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -3,5 +3,6 @@ export { UserIcon } from "./UserIcon";
 export { ArrowRight } from "./ArrowRight";
 export { CircleArrowsBottomIcon } from "./CircleArrowsBottomIcon";
 export { MoonIcon } from "./MoonIcon";
+export { BoltIcon } from "./BoltIcon";
 
 
