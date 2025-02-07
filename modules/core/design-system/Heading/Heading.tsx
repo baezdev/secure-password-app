fc6eff25eaa184fc092f789c352da3aa5b39af2c
baezdev <baezdev@gmail.com>
2025-02-07 00:30:35 -0600

    add new props to handle align and weight

--- modules/core/design-system/Heading/Heading.tsx
+++ modules/core/design-system/Heading/Heading.tsx
@@ -5,15 +5,38 @@ interface HeadingProps {
   children: React.ReactNode;
   variant: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   size?: "small" | "medium" | "large";
+  align?: "center" | "left" | "right";
+  weight?: "bold" | "semi-bold";
 }
 
-export function Heading({ size = "small", variant, children }: HeadingProps) {
+export function Heading({
+  size = "small",
+  variant,
+  children,
+  align = "left",
+  weight = "bold",
+}: HeadingProps) {
   const Tag = variant as keyof JSX.IntrinsicElements;
   const classSize = {
     small: css.Heading__small,
     medium: css.Heading__medium,
     large: css.Heading__large,
   };
+  const classAlign = {
+    center: css.Heading__center,
+    left: css.Heading__left,
+    right: css.Heading__right,
+  };
+  const classWeight = {
+    bold: css.Heading__bold,
+    "semi-bold": css.Heading__semiBold,
+  };
 
-  return <Tag className={`${css.Heading} ${classSize[size]}`}>{children}</Tag>;
+  return (
+    <Tag
+      className={`${css.Heading} ${classSize[size]} ${classAlign[align]} ${classWeight[weight]}`}
+    >
+      {children}
+    </Tag>
+  );
 }
