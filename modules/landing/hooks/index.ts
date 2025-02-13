388b4d4935da6465416ead908bb03d90cf2fcc71
baezdev <baezdev@gmail.com>
2025-02-12 21:39:12 -0600

    add useScrollVisibility hook for managing element visibility on scroll

--- modules/landing/hooks/index.ts
+++ modules/landing/hooks/index.ts
@@ -1,1 +1,2 @@
 export { useNav } from "./useNav"
+export { useScrollVisibility } from "./useScrollVisibility"
---
+++ modules/landing/hooks/useScrollVisibility.ts
@@ -0,0 +1,35 @@
+import { useEffect, useRef, useState } from "react";
+
+export function useScrollVisibility() {
+  const elementRef = useRef<HTMLDivElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.2,
+  };
+
+  const callbackFn = (entries: IntersectionObserverEntry[]) => {
+    const [entry] = entries;
+
+    if (entry.isIntersecting) {
+      setIsVisible(true);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callbackFn, options);
+    if (elementRef.current) observer.observe(elementRef.current);
+
+    return () => {
+      if (elementRef.current) observer.unobserve(elementRef.current);
+    };
+  }, [elementRef, options]);
+
+  return {
+    isVisible,
+    elementRef,
+  };
+}
