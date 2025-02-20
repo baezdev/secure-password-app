cd1c9c009ed4c004c0ea420cae5196fb0385310b
baezdev <baezdev@gmail.com>
2025-02-20 01:07:39 -0600

    style: update IconButton height for consistency and add MenuOpenIcon component

--- modules/core/components/Icons/Icons.tsx
+++ modules/core/components/Icons/Icons.tsx
@@ -13,9 +13,9 @@ export function Arrow({ color = "currentColor", size = 24 }: IconProps) {
       strokeLinecap="round"
       strokeLinejoin="round"
       className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-down"
->
-  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-  <path d="M6 9l6 6l6 -6" />
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M6 9l6 6l6 -6" />
     </svg>
   );
 }
@@ -196,6 +196,28 @@ export function UserIcon({ color = "currentColor", size = 24 }: IconProps) {
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
       <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+    </svg>
+  );
+}
+
+export function MenuOpenIcon({ color = "currentColor", size = 24 }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-menu-deep"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 6h16" />
+      <path d="M7 12h13" />
+      <path d="M10 18h10" />
     </svg>
   );
 }
